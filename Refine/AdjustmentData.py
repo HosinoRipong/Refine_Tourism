839fcfad0cc7d77620e22dcae3d579fb22db9853
Hyunsu Choi <mysj923@naver.com>
2024-04-11 01:53:56 +0900

    Add count_domestic_total

--- Refine/AdjustmentData.py
+++ Refine/AdjustmentData.py
@@ -11,4 +11,7 @@ data['with_leisure'] = data['with_leisure'].replace(6, 5)
 # 조정 3 : 19. 직업 선택요인 1순위
 data['factor_job'] = data['factor_job'].replace(9, 8)
 
+# 조정 4 : 12. 국내 관광여행 횟수(합) 추가
+data['count_domestic_total'] = data['count_domestic_lodgment'].fillna(0) + data['count_domestic_day'].fillna(0)
+
 data.to_csv('RefinedData.csv', index=False, encoding='cp949') # 데이터 저장 
